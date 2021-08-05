ab4ed00928446aafcfa9cf1ccb3b153944235bcd
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-08-05 12:05:17 +0700

    dev

--- examples/react/index.tsx
+++ examples/react/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import ReactSelect from 'react-select';
 import WebFormConstruct from '../../src/index';
+import ReactSelectBootstrapStyle from './ReactSelectBootstrapStyle';
 
 let render = (element, value, option?: any) => {
     let WebForm = WebFormConstruct({
@@ -63,30 +64,52 @@ let render = (element, value, option?: any) => {
         "checkbox": ({ name, readOnly, value }) => {
             return <></>;
         },
-        "select": ({ name, options, groupedOptions, readOnly, value,
+        "select": ({ name, readonly, value, label, error, placeholder, dataset, options, groupedOptions, readOnly,
             onChange }) => {
             if (readOnly) {
 
             } else {
-                return <select className="form-control rounded-0" name={name} value={value} onChange={onChange}>
-                    {options.map(k => <option value={k.value} key={k.value ?? k.label}>{k.label}</option>)}
-                </select>;
+                let requiredSign = dataset["data-validate-required"] ? <span className="text-danger">*</span> : <></>;
+                return <div className="form-floating">
+                    <select className="form-control rounded-0" name={name} value={value} onChange={onChange}>
+                        {options.map(k => <option value={k.value} key={k.value ?? k.label}>{k.label}</option>)}
+                    </select>
+                    {label &&
+                        <label>{label}&nbsp;{requiredSign}</label>
+                    }
+                </div>;
             }
         },
-        "reactselect": ({ name, options, groupedOptions, readOnly, value,
-            onChange }) => {
+        "reactselect": ({ name, readonly, value, label, error, placeholder, dataset, options, groupedOptions, readOnly, onChange }) => {
             if (readOnly) {
 
             } else {
-                let label = options.find(k => k.value == value)?.label;
-                return <ReactSelect
-                    value={{
-                        value: value,
-                        label: label
-                    }}
-                    onChange={onChange}
-                    options={options}
-                />;
+                let requiredSign = dataset["data-validate-required"] ? <span className="text-danger">*</span> : <></>;
+                let selectText = options.find(k => k.value == value)?.label;
+                return <div className="form-floating">
+                    <ReactSelect
+                        styles={{
+                            container: (provided) => {
+                                return {
+                                    ...provided,
+                                };
+                            },
+                            ...ReactSelectBootstrapStyle
+                        }}
+                        value={{
+                            value: value,
+                            label: selectText
+                        }}
+                        onChange={onChange}
+                        options={options}
+                    />
+                    {label &&
+                        <label style={{
+                            opacity: ".65",
+                            transform: "scale(.85) translateY(-.5rem) translateX(.15rem)"
+                        }}>{label}&nbsp;{requiredSign}</label>
+                    }
+                </div>;
             }
         },
         "row": ({ children }) => {
