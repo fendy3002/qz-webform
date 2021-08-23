92d2a9e83dce96eb493ef6d8f26c30a3b7fd45f3
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-08-23 13:37:54 +0700

    add editable to override readonly

--- src/helper/xmlToJson.ts
+++ src/helper/xmlToJson.ts
@@ -18,7 +18,9 @@ const xmlToJson = (xml, option) => {
             ...prop,
         };
         if (result.hasOwnProperty("readonly") || option.readOnly) {
-            result.readonly = true;
+            if(!result.hasOwnProperty("editable")){
+                result.readonly = true;
+            }
         } if (result.hasOwnProperty("checked")) {
             result.checked = true;
         } if (option.autoLabel && !result.label) {
