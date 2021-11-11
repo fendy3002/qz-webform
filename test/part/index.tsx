50b217424a2affa3ebf981e6ab5832be160d2d38
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-11-11 15:52:21 +0700

    dev

--- test/part/index.tsx
+++ test/part/index.tsx
@@ -1,7 +1,7 @@
 import * as types from '../../src/types';
 let customPartSet: types.Part.CustomPartSet = {
     "text": {
-        Component: ({ name, readonly, value, label, error, placeholder, dataset, required, hidden,
+        Component: ({ name, readonly, value, label, error, placeholder, required, hidden,
             onChange }) => {
             if (hidden) {
                 return <input type="hidden" name={name} value={value} />
@@ -9,7 +9,7 @@ let customPartSet: types.Part.CustomPartSet = {
             let requiredSign = required ? <span className="text-danger">*</span> : <></>;
             return <div className="form-floating">
                 <input type="text" className={"form-control rounded-0 " + (error ? "is-invalid" : "")} name={name}
-                    value={value} readOnly={readonly} placeholder={placeholder} {...dataset}
+                    value={value} readOnly={readonly} placeholder={placeholder}
                     onChange={onChange} />
                 {label &&
                     <label>{label}&nbsp;{requiredSign}</label>
@@ -23,14 +23,14 @@ let customPartSet: types.Part.CustomPartSet = {
         }
     },
     "textarea": {
-        Component: ({ name, readonly, value, label, error, placeholder, dataset, required, hidden,
+        Component: ({ name, readonly, value, label, error, placeholder, required, hidden,
             onChange }) => {
             let requiredSign = required ? <span className="text-danger">*</span> : <></>;
             let className = "form-control rounded-0 " + (error ? "is-invalid" : "");
 
             return <div className="form-floating ">
                 <textarea className={className} name={name}
-                    value={value} readOnly={readonly} placeholder={placeholder} {...dataset}
+                    value={value} readOnly={readonly} placeholder={placeholder}
                     style={{
                         height: "10em"
                     }}
@@ -45,6 +45,68 @@ let customPartSet: types.Part.CustomPartSet = {
                 }
             </div>;
         }
+    },
+    "number": {
+        Component: ({ name, readonly, value, label, error, placeholder, required,
+            onChange }) => {
+            let requiredSign = required ? <span className="text-danger">*</span> : <></>;
+            return <div className="form-floating">
+                <input type="text" className={"form-control rounded-0 " + (error ? "is-invalid" : "")} name={name}
+                    value={value} readOnly={readonly} placeholder={placeholder}
+                    onChange={onChange} />
+                {label &&
+                    <label>{label}&nbsp;{requiredSign}</label>
+                }
+                {error &&
+                    <div className="invalid-feedback">
+                        {error}
+                    </div>
+                }
+            </div>;
+        }
+    },
+    "checkbox": {
+        Component: ({ name, readonly, value, label, error, placeholder, onChange }) => {
+            return <div className="form-check form-switch">
+                <label className="form-check-label" >
+                    <input className="form-check-input" type="checkbox" name={name} checked={value} onChange={onChange}/>
+                    {label}
+                </label>
+            </div>;
+        }
+    },
+    "select": {
+        Component: ({ name, readonly, value, label, error, placeholder, options, groupedOptions, required,
+            onChange }) => {
+            if (readonly) {
+                let selectText = (options as any[]).find(k => k.value == value)?.label;
+                return <div className="form-floating">
+                    <input type="text" className={"form-control rounded-0 "}
+                        value={selectText} readOnly={true} placeholder={placeholder}
+                        onChange={onChange} />
+                    <input type="hidden" name={name} value={value} />
+                    {label &&
+                        <label>{label}</label>
+                    }
+                </div>;
+            } else {
+                let requiredSign = required ? <span className="text-danger">*</span> : <></>;
+                return <div className="form-floating">
+                    <select className={"form-control rounded-0 " + (error ? "is-invalid" : "")} name={name} value={value}
+                        onChange={onChange}>
+                        {(options as any[]).map(k => <option value={k.value} key={k.value ?? k.label}>{k.label}</option>)}
+                    </select>
+                    {label &&
+                        <label>{label}&nbsp;{requiredSign}</label>
+                    }
+                    {error &&
+                        <div className="invalid-feedback">
+                            {error}
+                        </div>
+                    }
+                </div>;
+            }
+        },
     }
 };
 
